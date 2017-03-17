b278380a48f159f2972ba235ccecee26c04daeca
Andrew <santechserp@gmail.com>
2017-03-17 19:26:39 +0300

    create

--- profile.php
+++ profile.php
@@ -96,13 +96,102 @@
                                     <a href="" class="post_data">17 марта 2017</a>
                                 </div>
                             </div>
-
                         </div>
-                        <div></div>
+                        <div class="post_text">
+                                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eius eos
+                                        eveniet facilis fugit magnam minus nam nihil nisi odio provident quia, quibusdam
+                                        quis quisquam reprehenderit sequi, ullam veritatis voluptas!
+                                    </div>
+                                    <div>Cupiditate dolor eaque fuga ipsa, quasi suscipit? Blanditiis deserunt esse est
+                                        fuga hic iure laudantium libero minus natus nihil, odio officiis, sapiente vel!
+                                        Dolore eos et impedit laudantium neque soluta.
+                                    </div>
+                                    <div>Assumenda delectus et illo natus nisi quasi qui tempore unde. Ex, quae,
+                                        voluptatum! Adipisci autem delectus dolore esse ex harum illo incidunt inventore
+                                        ipsa libero, nostrum, officia saepe similique voluptates!
+                                    </div>
+                                    <div>At quae, unde! Amet animi architecto aspernatur consequuntur delectus excepturi
+                                        hic illum iste, laudantium molestias nam obcaecati placeat quaerat quo sequi
+                                        tempore velit voluptas voluptatem? Ab aliquam animi deserunt iure?
+                                    </div>
+                                    <div>Dignissimos eum maxime porro quaerat recusandae. Adipisci aliquid, esse ipsam
+                                        maiores quae sunt veritatis voluptate! Dignissimos dolorem doloribus fuga, iusto
+                                        reiciendis voluptate! Animi et, fugiat nulla praesentium qui repellat
+                                        voluptatum.
+                                    </div>
+                        </div>
+
                     </div>
-                </div>
-
-
+                    <div class="bas-well">
+                        <div>
+                            <div class=""><a href=""><img  class="img-circle post_image_well" src="img/avatars/no_avatar.gif" alt=""/></a></div>
+                            <div>
+                                <div>
+                                    <a href="" class="post_author">Панин Андрей</a>
+                                </div>
+                                <div>
+                                    <a href="" class="post_data">17 марта 2017</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="post_text">
+                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eius eos
+                                eveniet facilis fugit magnam minus nam nihil nisi odio provident quia, quibusdam
+                                quis quisquam reprehenderit sequi, ullam veritatis voluptas!
+                            </div>
+                            <div>Cupiditate dolor eaque fuga ipsa, quasi suscipit? Blanditiis deserunt esse est
+                                fuga hic iure laudantium libero minus natus nihil, odio officiis, sapiente vel!
+                                Dolore eos et impedit laudantium neque soluta.
+                            </div>
+                            <div>Assumenda delectus et illo natus nisi quasi qui tempore unde. Ex, quae,
+                                voluptatum! Adipisci autem delectus dolore esse ex harum illo incidunt inventore
+                                ipsa libero, nostrum, officia saepe similique voluptates!
+                            </div>
+                            <div>At quae, unde! Amet animi architecto aspernatur consequuntur delectus excepturi
+                                hic illum iste, laudantium molestias nam obcaecati placeat quaerat quo sequi
+                                tempore velit voluptas voluptatem? Ab aliquam animi deserunt iure?
+                            </div>
+                            <div>Dignissimos eum maxime porro quaerat recusandae. Adipisci aliquid, esse ipsam
+                                maiores quae sunt veritatis voluptate! Dignissimos dolorem doloribus fuga, iusto
+                                reiciendis voluptate! Animi et, fugiat nulla praesentium qui repellat
+                                voluptatum.
+                            </div>
+                        </div>
+                        <div class="bas-well">
+                            <div>
+                                <div class=""><a href=""><img  class="img-circle post_image_well" src="img/avatars/no_avatar.gif" alt=""/></a></div>
+                                <div>
+                                    <div>
+                                        <a href="" class="post_author">Панин Андрей</a>
+                                    </div>
+                                    <div>
+                                        <a href="" class="post_data">17 марта 2017</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post_text">
+                                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eius eos
+                                    eveniet facilis fugit magnam minus nam nihil nisi odio provident quia, quibusdam
+                                    quis quisquam reprehenderit sequi, ullam veritatis voluptas!
+                                </div>
+                                <div>Cupiditate dolor eaque fuga ipsa, quasi suscipit? Blanditiis deserunt esse est
+                                    fuga hic iure laudantium libero minus natus nihil, odio officiis, sapiente vel!
+                                    Dolore eos et impedit laudantium neque soluta.
+                                </div>
+                                <div>Assumenda delectus et illo natus nisi quasi qui tempore unde. Ex, quae,
+                                    voluptatum! Adipisci autem delectus dolore esse ex harum illo incidunt inventore
+                                    ipsa libero, nostrum, officia saepe similique voluptates!
+                                </div>
+                                <div>At quae, unde! Amet animi architecto aspernatur consequuntur delectus excepturi
+                                    hic illum iste, laudantium molestias nam obcaecati placeat quaerat quo sequi
+                                    tempore velit voluptas voluptatem? Ab aliquam animi deserunt iure?
+                                </div>
+                                <div>Dignissimos eum maxime porro quaerat recusandae. Adipisci aliquid, esse ipsam
+                                    maiores quae sunt veritatis voluptate! Dignissimos dolorem doloribus fuga, iusto
+                                    reiciendis voluptate! Animi et, fugiat nulla praesentium qui repellat
+                                    voluptatum.
+                                </div>
+                            </div>
 			</div>
 		</section>
 <?php
