b4df880694782c99ad855f46b642e940da4bf722
Andrew <santechserp@gmail.com>
2017-03-17 12:42:05 +0300

    обновление

--- view_helpers/header.php
+++ view_helpers/header.php
@@ -14,7 +14,7 @@ echo '
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>FLUFFYLAND</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
