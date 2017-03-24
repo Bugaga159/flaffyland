7727086338d9c9e513784464ec96e24633ffe8f8
Andrew <santechserp@gmail.com>
2017-03-25 02:10:45 +0300

    add

--- messeges.php
+++ messeges.php
@@ -1,7 +1,175 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Andrew
- * Date: 25.03.2017
- * Time: 1:28
- */
+    $btn_head = 'exit_head_btn';
+
+    require_once "view_helpers/header.php";
+
+?>
+
+
+    <section class="container content">
+
+        <!--------------------------------------   Навигация    --------------------------------->
+
+        <div class="col-md-2 user_menu">
+            <ul class="user_menu">
+                <li><a class="user_menu_link" href="profile.php"><div class="menu_icon glyphicon glyphicon-home"></div><p class="user_menu_text" href="#">Главная</p></a></li>
+                <li><a class="user_menu_link" href="messeges.php"><div class="menu_icon glyphicon glyphicon-envelope"></div><p class="user_menu_text" href="#">Сообщения</p></a></li>
+                <li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon glyphicon-user"></div><p class="user_menu_text" href="#">Друзья</p></a></li>
+                <li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-picture"></div><p class="user_menu_text" href="#">Фото</p></a></li>
+                <li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-comment"></div><p class="user_menu_text" href="#">Новости</p></a></li>
+                <li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-cog"></div><p class="user_menu_text" href="#">Изменить</p></a></li>
+                <li><a class=" " href="#"><div class="menu_icon glyphicon glyphicon-globe"></div><p class="user_menu_text" href="#">Группы</p></a></li>
+            </ul>
+        </div>
+
+        <!---------------------------------     Сообщение -------------------------------------->
+
+        <div class="col-md-10">
+
+            <div>
+                <a href="#" class="search-mess"><div class="menu_icon glyphicon glyphicon-search"></div>Поиск переписки</a>
+            </div>
+
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+            <div>
+                <div class="mess-post">
+                    <a href="">
+                        <img  class="foto-messeges" src="img/avatars/ava-i.jpg" alt="">
+                        <span  href="#" class="foto-messeges-name">Панин Андрей</span>
+                    </a>
+                </div>
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+<?php
+require_once "view_helpers/footer.php";
+?>
