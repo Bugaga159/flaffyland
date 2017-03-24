d920b9fc276281abe8c6596fbdcf1302fd58c024
Andrew <santechserp@gmail.com>
2017-03-25 01:28:35 +0300

    add messeges.php

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@
 		<section class="container content">
 			<div class="col-md-2 user_menu">
 				<ul class="user_menu">
-					<li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-home"></div><p class="user_menu_text" href="#">Главная</p></a></li>
+					<li><a class="user_menu_link" href="profile.php"><div class="menu_icon glyphicon glyphicon-home"></div><p class="user_menu_text" href="#">Главная</p></a></li>
 					<li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-envelope"></div><p class="user_menu_text" href="#">Сообщения</p></a></li>
 					<li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon glyphicon-user"></div><p class="user_menu_text" href="#">Друзья</p></a></li>
 					<li><a class="user_menu_link" href="#"><div class="menu_icon glyphicon glyphicon-picture"></div><p class="user_menu_text" href="#">Фото</p></a></li>
